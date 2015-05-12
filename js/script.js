f34d4c5ca5821e9fff479622cb44e71e75a39eab
ericleoo <ericl619@gmail.com>
2015-05-12 16:16:11 +0800

    Bootcamp day2 update
    HTML5, CSS3, and APIs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 	var inputBox = document.getElementById('inputBox');
 	var sendButton = document.getElementsByTagName('button')[0];
+	var clearButton = document.getElementsByTagName('button')[1];
 	var messageBox = document.querySelector('#messageBox');
 	
 	function addMessage(){
@@ -10,7 +11,16 @@ document.addEventListener('DOMContentLoaded', function(){
 		inputBox.value = '';
 	};
 	
+	function clearMessage(){
+		inputBox.value = '';
+		
+		while (messageBox.firstChild) 
+			messageBox.removeChild(messageBox.firstChild);
+	};
+	
 	sendButton.addEventListener('click',addMessage);
+	clearButton.addEventListener('click',clearMessage);
+	
 	inputBox.addEventListener('keyup',function(e){
 		if(e.keyCode === 13){
 			addMessage();
